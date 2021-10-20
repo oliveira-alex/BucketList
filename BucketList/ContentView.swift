196e1d8b4bc45e712716e54c9cae201156626261
ALeX <alexdeoliveira.poli@gmail.com>
2021-10-20 20:15:26 -0300

    Writing data to the documents directory – Bucket List SwiftUI Tutorial 2/13

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -2,38 +2,38 @@
 //  ContentView.swift
 //  BucketList
 //
-//  Created by Alex Oliveira on 20/10/21.
+//  Created by Alex Oliveira on 20/10/2021.
 //
 
 import SwiftUI
 
-struct User: Identifiable, Comparable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-    
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
-
 struct ContentView: View {
-//    let values = [1, 5, 3, 6, 2, 9].sorted()
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister")
-    ].sorted ()
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
     
     var body: some View {
-        List(users) { user in
-            Text("\(user.lastName), \(user.firstName)")
-        }
+        Text("Hello, world!")
+            .padding()
+            .onTapGesture {
+                let str = "Test Message"
+                let url = self.getDocumentsDirectory().appendingPathComponent("message.txt")
+
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
